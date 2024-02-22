dc0e1d1d4c1b78d4a5d7f9a1e1b35ce98067a462
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-02-22 14:38:32 +0530

    added axios and testing of commit

--- src/components/TopicWiseQuestion/TopicWiseQuestion.jsx
+++ src/components/TopicWiseQuestion/TopicWiseQuestion.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './TopicWiseQuestion.css'
+import React from "react";
+import "./TopicWiseQuestion.css";
+import { Button } from "@mui/material";
+import axios from "axios";
+import { API } from "../../utils/constant";
 const TopicWiseQuestion = () => {
+  const fetchAllQuestion = async () => {
+    try {
+      const response = await axios.get(`${API}/all-questions`);
+      console.log("response", response);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <div className="container">
-        
+      <div className="col-md-6">
+        <Button onClick={() => fetchAllQuestion()}>Fetch Question</Button>
+      </div>
+      <div className="col-md-6"></div>
     </div>
-  )
-}
+  );
+};
 
-export default TopicWiseQuestion
+export default TopicWiseQuestion;
