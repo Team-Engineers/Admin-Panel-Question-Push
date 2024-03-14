5901df5067b4fe0d37c8380e573b60f50503a279
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2024-03-14 18:49:54 +0530

    trimmed value before preview

--- src/components/MathJax/MathText.jsx
+++ src/components/MathJax/MathText.jsx
@@ -8,6 +8,8 @@ export const MathText = ({ text, textTag = "p" }) => {
     text = text.toString();
   }
 
+  text = text.trim();
+
   const parts = text.split(/\n/);
 
   const jsxElements = parts.flatMap((part, index) => {
@@ -18,20 +20,22 @@ export const MathText = ({ text, textTag = "p" }) => {
     if (hasMathExpression) {
       const subparts = part.split(/\$(.*?)\$/);
 
-      return subparts.map((subpart, subIndex) => {
-        if (subIndex % 2 !== 0) {
-          return (
-            <MathComponent
-              key={`${index}_${subIndex}`}
-              tex={subpart}
-              display={false}
-              className="math-expression"
-            />
-          );
-        } else {
-          return <span key={`${index}_${subIndex}`}>{subpart}</span>;
-        }
-      }).concat(<br key={`br_${index}`} className="minimal-space" />);
+      return subparts
+        .map((subpart, subIndex) => {
+          if (subIndex % 2 !== 0) {
+            return (
+              <MathComponent
+                key={`${index}_${subIndex}`}
+                tex={subpart}
+                display={false}
+                className="math-expression"
+              />
+            );
+          } else {
+            return <span key={`${index}_${subIndex}`}>{subpart}</span>;
+          }
+        })
+        .concat(<br key={`br_${index}`} className="minimal-space" />);
     } else if (hasHTMLTags) {
       return [
         <TextTag
